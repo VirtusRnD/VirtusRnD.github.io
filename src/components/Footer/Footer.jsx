d69c7fce623d534aa7fddbb5fe0bc760b4d3683a
isminiz <canince66@gmail.com>
2024-03-23 14:21:55 +0300

    minor changes added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import "./Footer.css";
 import { translate } from "../Translation/Translation";
 
@@ -15,23 +15,45 @@ const Footer = () => {
     const aboutus = translate("aboutus");
     const career = translate("career");
     const contact = translate("contact");
+    const [selectedLang, setSelectedLang] = useState("tr");
 
+    useEffect(() => {
+        const handleLanguageChangeFromUrl = () => {
+          const urlParams = new URLSearchParams(window.location.search);
+          const lang = urlParams.get("lang");
+          if (lang) {
+            setSelectedLang(lang);
+          }
+        };
+    
+        handleLanguageChangeFromUrl();
+    
+      }, []);
+
+      const handleNavLinkClick = (sectionId) => {
+        document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+        
+      };
     return (
         <div className="footer">
             <div className="sb__footer section__padding">
                 <div className="sb__footer-links">
                     <div className="sb__footer-links__div">
                         <h4 className='footer-h4'>{products}</h4>
-                        <a href = "https://virtusarge.com/#about">
+                        <a onClick={() => handleNavLinkClick("testpage")}>
+                            <span className="nav-link"></span>
                             <p>{production_web}</p>
                         </a>
-                        <a href = "https://virtusarge.com/#about">
+                        <a onClick={() => handleNavLinkClick("testpage")}>
+                            <span className="nav-link"></span>
                             <p>{production_mobile}</p>
                         </a>
-                        <a href = "https://virtusarge.com/#about">
+                        <a onClick={() => handleNavLinkClick("testpage")}>
+                            <span className="nav-link"></span>
                             <p>{production_data}</p>
                         </a>
-                        <a href = "https://virtusarge.com/#about">
+                        <a onClick={() => handleNavLinkClick("testpage")}>
+                            <span className="nav-link"></span>
                             <p>{production_digitale}</p>
                         </a>
                     </div>
@@ -39,13 +61,15 @@ const Footer = () => {
 
                     <div className="sb__footer-links__div">
                         <h4 className='footer-h4'>{company}</h4>
-                        <a href = "https://virtusarge.com/#about">
+                        <a onClick={() => handleNavLinkClick("about")}>
+                            <span className="nav-link"></span>
                             <p>{aboutus}</p>
                         </a>
                         <a href = "https://www.linkedin.com/company/virtusarge/">
                             <p>{career}</p>
                         </a>
-                        <a href = "https://virtusarge.com/#contact">
+                        <a onClick={() => handleNavLinkClick("contact")}>
+                            <span className="nav-link"></span>
                             <p>{contact}</p>
                         </a>
                     </div>
